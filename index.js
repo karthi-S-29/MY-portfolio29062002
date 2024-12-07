c7e33f999f2028dd624a5dd3615c0c5c62044316
karthi-S-29 <sskarthi2962002@gmail.com>
2024-12-07 14:26:44 +0530

    new
    contant upadate

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function hidesidebar(){
     const sidebar=document.querySelector('.sidebar')
     sidebar.style.display='none';
 }
+
+
+var typed = new Typed('.typewriter1', {
+    strings: ['Frontend Developer', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
